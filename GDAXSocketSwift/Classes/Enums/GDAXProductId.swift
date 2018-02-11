282937d5c9de31ad93f0308d390856a8c544b5de
Russ d'Sa <russelldsa@gmail.com>
2018-02-11 13:57:05 -0800

    Update GDAXProductId.swift

--- GDAXSocketSwift/Classes/Enums/GDAXProductId.swift
+++ GDAXSocketSwift/Classes/Enums/GDAXProductId.swift
@@ -18,4 +18,7 @@ public enum GDAXProductId: String {
     case LTCUSD = "LTC-USD"
     case LTCBTC = "LTC-BTC"
     case LTCEUR = "LTC-EUR"
+    case BCHUSD = "BCH-USD"
+    case BCHBTC = "BCH-BTC"
+    case BCHEUR = "BCH-EUR"
 }
